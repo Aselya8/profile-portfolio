deb75ba6c4c3f1b21a44c698f37f3a548adc3900
Asel Atakeev <aselya.atakeeva@gmail.com>
2022-06-19 12:43:59 -0700

    About me and Edu/components

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,30 +5,17 @@ const Education = () => {
     return (
         <div id="education" className="education">
             <div className="d-flex justify-content-center my-5">
-                <h1>education</h1>
+                <h1>education & Courses</h1>
             </div>
             <div className="container education-wrapper">
                 <div className="timeline-block timeline-block-left">
                     <div className="marker"></div>
                     <div className="timeline-content">
                     <h3>Polytechnic College of International University of Kyrgyzstan</h3>
-                    <p>
-                    <br /> Communication Systems and Networks
-                    <br /> - Bachelor of Computer Science
+                    <p> Communication Systems and Networks
+                    <br /> September 2006 — June 2009
+                    <br />
                     <br /> 
-                    </p>
-                    
-                </div>
-                </div>
-
-                <div className="timeline-block timeline-block-right">
-                    <div className="marker"></div>
-                    <div className="timeline-content">
-                    <h3>Nucamp Coding Bootcamp/San Francisco,CA</h3>
-                    <p>  
-                    <br /> - Web Development Fundamentals - Certificate 
-                    <br /> - Front End Web + Mobile Developer - Certificate 
-                    <br /> - Full Stack Web and Mobile App Developer - Certificate
                     </p>
                     </div>
                 </div>
@@ -37,11 +24,22 @@ const Education = () => {
                     <div className="marker"></div>
                     <div className="timeline-content">
                     <h3>Diablo Valley College/Pleasant Hill,CA</h3>
-                    <p> - - Computer Information Systems
-                    <br /> - In progress
+                    <p> Computer Information Systems
+                    <br /> January 2020 - Present
                     </p>
                     </div>
                 </div>
+
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                    <h3>Nucamp Coding Bootcamp/San Francisco,CA</h3>
+                    <p> Full Stack Web and Mobile App Developer
+                    <br /> May 2021 - November 2021 
+                    </p>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
