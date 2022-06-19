ab0dc8f22df37ff167c4ac23466bfedd617cd7a8
Asel Atakeev <aselya.atakeeva@gmail.com>
2022-06-19 12:48:38 -0700

    right-left

--- src/components/Education.js
+++ src/components/Education.js
@@ -20,7 +20,7 @@ const Education = () => {
                     </div>
                 </div>
 
-                <div className="timeline-block timeline-block-left">
+                <div className="timeline-block timeline-block-right">
                     <div className="marker"></div>
                     <div className="timeline-content">
                     <h3>Diablo Valley College/Pleasant Hill,CA</h3>
@@ -30,7 +30,7 @@ const Education = () => {
                     </div>
                 </div>
 
-                <div className="timeline-block timeline-block-right">
+                <div className="timeline-block timeline-block-left">
                     <div className="marker"></div>
                     <div className="timeline-content">
                     <h3>Nucamp Coding Bootcamp/San Francisco,CA</h3>
