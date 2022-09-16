a55e87b423dbfee5a2898d3c848fc01d7d8a6129
Asel Atakeev <aselya.atakeeva@gmail.com>
2022-09-16 12:26:32 -0700

    added an new certificate

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -2,6 +2,8 @@ import React from "react";
 import certificate1 from "../images/certificate1.png";
 import certificate2 from "../images/certificate2.png";
 import certificate3 from "../images/certificate3.png";
+//added an new certificate
+import certificate3 from "../images/certificate4.png";
 // REACT POPUPBOX
 import { PopupboxManager, PopupboxContainer } from "react-popupbox";
 import "react-popupbox/dist/react-popupbox.css";
@@ -90,22 +92,58 @@ const Certificates = () => {
   
   //
 
+
+  // certificate4
+  const openPopupboxCertificate4 = () => {
+    const content = (
+      <>
+        <img className="certificate-image-popupbox" src={certificate3} alt="Certificate4..." />
+      </>
+    )
+    PopupboxManager.open({ content });
+    PopupboxManager.update({  content, config: {
+      titleBar: {  text: "Application Developmenr - JAVA Intensive"  }
+},
+});
+  }
+
+  const popupboxConfigCertificate4 = {
+    titleBar: {
+      enable: true,
+      text: "Application Developmenr - JAVA Intensive"
+    },
+    fadeIn: true,
+    fadeInSpeed: 500
+  }
+  
+  //
+
     return (
         <div id="certificate" className="certificate-wrapper">
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">Certificates</h1>
                 <div className="image-box-wrapper row row-cols-auto justify-content-center">
+                
                     <div className="certificate-image-box" onClick={openPopupboxCertificate1}>
                         <img className="certificate-image" src={certificate1} alt="Certificate1..." />
                         <div className="certificate-overflow"></div>
                         <FontAwesomeIcon className="certificate-icon" icon={faSearchPlus} />
                     </div>
+
                     {/* - */}
                     <div className="certificate-image-box" onClick={openPopupboxCertificate2}>
                         <img className="certificate-image" src={certificate2} alt="Certificate2..." />
                         <div className="certificate-overflow"></div>
                         <FontAwesomeIcon className="certificate-icon" icon={faSearchPlus} />
                     </div>
+
+                    {/* - */}
+                    <div className="certificate-image-box" onClick={openPopupboxCertificate3}>
+                        <img className="certificate-image" src={certificate3} alt="Certificate3..." />
+                        <div className="certificate-overflow"></div>
+                        <FontAwesomeIcon className="certificate-icon" icon={faSearchPlus} />
+                    </div>
+
                     {/* - */}
                     <div className="certificate-image-box" onClick={openPopupboxCertificate3}>
                         <img className="certificate-image" src={certificate3} alt="Certificate3..." />
@@ -117,6 +155,7 @@ const Certificates = () => {
             <popupboxConfigCertificate1 {...popupboxConfigCertificate1} />
             <PopupboxContainer {...popupboxConfigCertificate2} />
             <PopupboxContainer {...popupboxConfigCertificate3} />
+            <PopupboxContainer {...popupboxConfigCertificate4} />
         </div>
 
     )
